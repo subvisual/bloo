1b4d0e31ac48f0cd6a614dd4ec046c24ceb43b8c
Fernando Mendes <devfrmendes@gmail.com>
2017-01-10 16:55:55 +0000

    Remove dotenv from production

--- app/boot.rb
+++ app/boot.rb
@@ -3,15 +3,17 @@ $LOAD_PATH.unshift File.expand_path(File.dirname(__FILE__))
 require "sinatra"
 require "active_record"
 require "active_support/all"
-require "dotenv"
 require "json"
 require "slack-ruby-client"
+
+unless Sinatra::Base.production?
+  require "dotenv"
+  Dotenv.load
+end
 
 require "emilie"
 require "slack"
 
 require "app"
-
-Dotenv.load unless Sinatra::Base.production?
 
 ActiveRecord::Base.establish_connection(Emilie.config[:active_record])
